8c19f154d2a42260e32a9294742b5ccb58b9158a
Anastasiazhukova22 <Anastasiakinski2@gmail.com>
2024-12-17 19:45:04 +0300

    user controller get and post

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -11,19 +11,24 @@ import java.util.List;
 
 public class UserController {
 
-    List<UserModel> UserList = new ArrayList<>();
+    List<UserModel> userList = new ArrayList<>();
 
 
     @PostMapping("/user/add")
     public String addUser (@RequestBody UserDto userModel) {
-        UserModel usersmodel = UserMapper.map(userModel);
-        userList.add(userModel);
+        UserModel usermodel = UserMapper.map(userModel);
+        userList.add(usermodel);
 
         System.out.println(userModel);
-        return "На сайт добавлен новый пользователь с ID" + usersmodel.userId();
+        return "На сайт добавлен новый пользователь с ID" + usermodel.userId();
     }
 
+    @GetMapping("/user/get")
+    public UserModel getuser(Integer userId) {
+
+        return userList.stream().filter(u ->userId.equals(u.userId())).findAny().get();
     }
+
 }
 
 
