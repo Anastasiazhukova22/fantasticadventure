d9df8351b9a67f6f54374219f089e14092aa2c02
Anastasiazhukova22 <Anastasiakinski2@gmail.com>
2024-12-17 19:22:47 +0300

    user controller

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,12 +1,29 @@
 package org.example.controllers;
 
-import org.example.dto.ArtistContent;
-import org.example.dto.ArtistContentDto;
-import org.example.mappers.ArtistMapper;
+import org.example.dto.UserDto;
+import org.example.dto.UserModel;
+import org.example.mappers.UserMapper;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+@RestController
+
 public class UserController {
+
+    List<UserModel> UserList = new ArrayList<>();
+
+
+    @PostMapping("/user/add")
+    public String addUser (@RequestBody UserDto userModel) {
+        UserModel usersmodel = UserMapper.map(userModel);
+        userList.add(userModel);
+
+        System.out.println(userModel);
+        return "На сайт добавлен новый пользователь с ID" + usersmodel.userId();
+    }
+
+    }
 }
+
+
